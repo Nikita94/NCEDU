41922b4b44ebea29c8943e47ced0d02216563318
Sirotin Nikita <niksir20@mail.ru>
2016-03-09 17:54:22 +0300

    Update main

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -12,26 +12,7 @@ import java.util.List;
  */
 public class Server {
     public static void main (String argv[]) throws Exception {
-       Factory factory = Factory.getInstance();
-       ParentJarAndHisDependencies navD = factory.getJarNameAndVersionDao();
-      // List<String> list = new ArrayList<String>(navD.getAllDependencies("TestNameA v1.0.0.2"));
-      // navD.removeSetDependencies();
-      // List<String> list2 = new ArrayList<String>(navD.getAllDependencies("DependeceWithDependecies v3.0.0.2"));
-      // toJSON json = new toJSON(list);
-      // json.createJSON();
-
         SocketServer socketServer = new SocketServer();
         socketServer.createSocket();
-     // List<String> list = new ArrayList<>();
-     // list.add("DependeceWithDependecies v3.0.0.2");
-     // list.add("Dependece1_1 v4.1");
-     // list.add("Dependece1_2 v2.2.1");
-     // list.add("Dependece2 v1.2.1");
-     // list.add("Dependece2_1 v3.1");
-     // list.add("Dependece1 v1.1");
-     // list.add("Dependencies2_1_1 v.12.3");
-     // DependenciesJars dependenciesJars = navD.checkDependencies(list);
-     // toJSON tojson = new toJSON(dependenciesJars);
-     // tojson.createJSON();
     }
 }
