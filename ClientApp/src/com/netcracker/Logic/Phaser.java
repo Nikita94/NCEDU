0aa29a20b167cd42982ac6761531dd384bd951b4
Maxim Tsvetkov <max.nnsu@gmail.com>
2016-03-14 17:34:46 +0300

    Castle xD

--- ClientApp/src/com/netcracker/Logic/Phaser.java
+++ ClientApp/src/com/netcracker/Logic/Phaser.java
@@ -2,15 +2,34 @@ package com.netcracker.Logic;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Admin on 3/12/2016.
  */
 public class Phaser {
     private String ans;
+    ArrayList<String> fin=new ArrayList<String>();
+
+//{"dependenciesJars":{"infoAboutDependencies":"All dependencies are met","setDep":[]}}
 
     public void result(String out){
-        Gson transform=new Gson();
-        transform.toJson(out);
+        Pattern pattern= Pattern.compile("[(\"[a-zA-Z]\\s+\")]+");
+
+        Matcher m = pattern.matcher(out);
+            while (m.find()) {
+                fin.add(m.group());
+               // System.out.println(m.group());
+            }
+            fin.remove(0);
+            fin.remove(0);
+            System.out.println(fin.get(0));
+
+
+
+
 
 
     }
